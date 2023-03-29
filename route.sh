ed2cfdf34745c3d17dfd028580030d9ba3421a90
Jens Gerke <jens.gerke@direktiv.io>
2023-03-29 15:16:50 +0200

    realm fix
    Signed-off-by: Jens Gerke <jens.gerke@direktiv.io>

--- route.sh
+++ route.sh
@@ -9,7 +9,7 @@ curl http://127.0.0.1:9180/apisix/admin/routes/6 -H 'X-API-KEY: changeme' -X PUT
       "client_secret": "KN7ac3NcvS9ihwmXZJMPBexnEKlxX7H2",
       "discovery": "http://127.0.0.1:8080/realms/demo/.well-known/openid-configuration",
       "bearer_only": false,
-      "realm": "master",
+      "realm": "demo",
       "redirect_uri": "/secure/redirect_uri",
       "post_logout_redirect_uri": "http://127.0.0.1:9080/index.html",
       "logout_path": "/secure/logout"
